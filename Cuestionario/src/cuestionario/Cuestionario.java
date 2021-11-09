7b10e42bfc813d3ece0d5fd0e3b66d85808c0373
Carlos Castillo <robocarlos10@gmail.com>
2021-11-09 12:28:27 +0100

    Cuestionario empezado terminar preguntas 2

---
+++ Cuestionario/src/cuestionario/Cuestionario.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cuestionario;
+
+/**
+ *
+ * @author Manuel
+ *
+ */
+public class Cuestionario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Contador contador = new Contador();
+        System.out.println("Este cuestionario es sobre el juego OGAME.");
+        System.out.println("En el deberas de poner informacion como (nombre de jugador, numero de planetas colonizados, " +
+                "cantidad de recursos y muchos mas.)");
+        System.out.println();
+    }
+
+}
